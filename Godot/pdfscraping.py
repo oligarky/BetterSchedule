b0bf6bb75fab31f836cc3b6df82fe2e6f4d3f1f3
Thomas Powell <powellt1@wit.edu>
2023-04-10 16:20:48 -0400

    Added python and scraper

---
+++ Godot/pdfscraping.py
@@ -0,0 +1,14 @@
+import PyPDF2
+from PyPDF2 import PdfReader
+
+reader = PdfReader("DegreeAudit.pdf")
+number_of_pages = len(reader.pages)
+parts = []
+
+
+with open("Output.txt", "a") as f:
+    for i in range(1,number_of_pages) :
+        page = reader.pages[i]
+        text= page.extract_text()
+        print(text, file=f)
+         
